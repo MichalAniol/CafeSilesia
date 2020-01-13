0bc686e595e8a4fad537c4009e1be5adf0d8c9b3
Michał Anioł <michal.janusz.aniol@gmail.com>
2020-01-13 12:30:12 +0100

    1st

---
+++ data.js
@@ -0,0 +1,324 @@
+const data_1 = [{
+    n: 'Strona głowna',
+    c: [
+        {
+            n: 'Kawa',
+            c: [
+                {
+                    n: 'Rodzaj kawy',
+                    c: [
+                        {
+                            n: 'Kawa ziarnista',
+                            c: [
+                                {
+                                    n: 'kawa nr 1'
+                                },
+                                {
+                                    n: 'kawa nr 2'
+                                },
+                                {
+                                    n: 'kawa nr 3'
+                                },
+                                {
+                                    n: '...'
+                                }
+                            ]
+                        },
+                        {
+                            n: 'Kawa mielona',
+                            c: [
+                                {
+                                    n: 'kawa nr 1'
+                                },
+                                {
+                                    n: 'kawa nr 2'
+                                },
+                                {
+                                    n: 'kawa nr 3'
+                                },
+                                {
+                                    n: '...'
+                                }
+                            ]
+                        },
+                        {
+                            n: 'Kawa w kapsułkach',
+                            c: [
+                                {
+                                    n: 'kawa nr 1'
+                                },
+                                {
+                                    n: 'kawa nr 2'
+                                },
+                                {
+                                    n: 'kawa nr 3'
+                                },
+                                {
+                                    n: '...'
+                                }
+                            ]
+                        },
+                        {
+                            n: '...'
+                        },
+                    ]
+                },
+                {
+                    n: 'Przeznaczenie Kawy',
+                    c: [
+                        {
+                            n: 'do ekspresu automatycznego',
+                            c: [
+                                {
+                                    n: 'kawa nr 1'
+                                },
+                                {
+                                    n: 'kawa nr 2'
+                                },
+                                {
+                                    n: 'kawa nr 3'
+                                },
+                                {
+                                    n: '...'
+                                }
+                            ]
+                        },
+                        {
+                            n: 'do ekspresu kolbowego',
+                            c: [
+                                {
+                                    n: 'kawa nr 1'
+                                },
+                                {
+                                    n: 'kawa nr 2'
+                                },
+                                {
+                                    n: 'kawa nr 3'
+                                },
+                                {
+                                    n: '...'
+                                }
+                            ]
+                        },
+                        {
+                            n: 'do kawiarki',
+                            c: [
+                                {
+                                    n: 'kawa nr 1'
+                                },
+                                {
+                                    n: 'kawa nr 2'
+                                },
+                                {
+                                    n: 'kawa nr 3'
+                                },
+                                {
+                                    n: '...'
+                                }
+                            ]
+                        },
+                        {
+                            n: '...'
+                        }
+                    ]
+                },
+                {
+                    n: 'Palarnia',
+                    c: [
+                        {
+                            n: 'Anglia'
+                        },
+                        {
+                            n: 'Hiszpania'
+                        },
+                        {
+                            n: 'Kolumbia'
+                        },
+                        {
+                            n: '...'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            n: 'Herbaty',
+            c: [
+                {
+                    n: 'Rodzaj herbaty'
+                },
+                {
+                    n: 'Typ herbaty'
+                },
+                {
+                    n: 'Producent'
+                },
+                {
+                    n: 'Przeznaczenie'
+                }
+            ]
+        }
+    ]
+}]
+
+const data_2 = [{
+    n: 'Strona głowna',
+    c: [{
+        n: 'Przeznaczenie Kawy',
+        c: [
+            {
+                n: 'do ekspresu automatycznego',
+                c: [
+                    {
+                        n: 'kawa nr 1'
+                    },
+                    {
+                        n: 'kawa nr 2'
+                    },
+                    {
+                        n: 'kawa nr 3'
+                    },
+                    {
+                        n: '...'
+                    }
+                ]
+            },
+            {
+                n: 'do ekspresu kolbowego',
+                c: [
+                    {
+                        n: 'kawa nr 1'
+                    },
+                    {
+                        n: 'kawa nr 2'
+                    },
+                    {
+                        n: 'kawa nr 3'
+                    },
+                    {
+                        n: '...'
+                    }
+                ]
+            },
+            {
+                n: 'do kawiarki',
+                c: [
+                    {
+                        n: 'kawa nr 1'
+                    },
+                    {
+                        n: 'kawa nr 2'
+                    },
+                    {
+                        n: 'kawa nr 3'
+                    },
+                    {
+                        n: '...'
+                    }
+                ]
+            },
+            {
+                n: '...'
+            }
+        ]
+    },
+    {
+        n: 'Palarnia',
+        c: [
+            {
+                n: 'Anglia'
+            },
+            {
+                n: 'Hiszpania'
+            },
+            {
+                n: 'Kolumbia'
+            },
+            {
+                n: '...'
+            }
+        ]
+    },
+    {
+        n: 'Kawa',
+        c: [
+            {
+                n: 'Rodzaj kawy',
+                c: [
+                    {
+                        n: 'Kawa ziarnista',
+                        c: [
+                            {
+                                n: 'kawa nr 1'
+                            },
+                            {
+                                n: 'kawa nr 2'
+                            },
+                            {
+                                n: 'kawa nr 3'
+                            },
+                            {
+                                n: '...'
+                            }
+                        ]
+                    },
+                    {
+                        n: 'Kawa mielona',
+                        c: [
+                            {
+                                n: 'kawa nr 1'
+                            },
+                            {
+                                n: 'kawa nr 2'
+                            },
+                            {
+                                n: 'kawa nr 3'
+                            },
+                            {
+                                n: '...'
+                            }
+                        ]
+                    },
+                    {
+                        n: 'Kawa w kapsułkach',
+                        c: [
+                            {
+                                n: 'kawa nr 1'
+                            },
+                            {
+                                n: 'kawa nr 2'
+                            },
+                            {
+                                n: 'kawa nr 3'
+                            },
+                            {
+                                n: '...'
+                            }
+                        ]
+                    },
+                    {
+                        n: '...'
+                    },
+                ]
+            }
+        ]
+    },
+    {
+        n: 'Herbaty',
+        c: [
+            {
+                n: 'Rodzaj herbaty'
+            },
+            {
+                n: 'Typ herbaty'
+            },
+            {
+                n: 'Producent'
+            },
+            {
+                n: 'Przeznaczenie'
+            }
+        ]
+    }
+    ]
+}]
---
+++ index.js
@@ -0,0 +1,110 @@
+const diagram = document.getElementById('diagram'),
+    breadcrumbs = document.querySelector('.breadcrumbs'),
+    nodeH = 26;
+var currentItem = diagram;
+
+function createItem(parent, child) {
+    let item = document.createElement('div');
+    item.innerHTML = child.n;
+    item.className = 'node';
+
+    parent.appendChild(item);
+
+    return item;
+}
+
+function goDeeper(parent, child) {
+    for (let c of child) {
+        let p = createItem(parent, c)
+        if (c.c) {
+            goDeeper(p, c.c)
+        }
+    }
+}
+
+goDeeper(diagram, data_1)
+
+function checkHeight(item) {
+    let h = item.getBoundingClientRect().height - 10;
+    item.setAttribute('data-heightmemo', h)
+    item.style.height = h + 'px';
+
+    if (item.childNodes) {
+        for (let c of item.childNodes) {
+            if (c.tagName == 'DIV') {
+                checkHeight(c)
+            }
+        }
+    }
+}
+
+checkHeight(diagram)
+
+document.querySelector('.wrap').addEventListener('click', (e) => {
+    function getName(item) {
+        let inner = item.innerHTML,
+            end = inner.indexOf('<');
+        if (end == -1) {
+            return inner
+        }
+        return inner.slice(0, end);
+    }
+
+    function getParent(item) {
+        return item.parentElement
+    }
+
+    let name = getName(e.target),
+        node = e.target,
+        parent = getParent(node);
+
+    while (parent.id != 'diagram') {
+        name = getName(parent) + ': ' + name;
+        node = parent;
+        parent = getParent(node);
+    }
+
+    breadcrumbs.innerHTML = name;
+
+    setColor(e.target)
+})
+
+function setColor(item) {
+    if (item != currentItem) {
+        item.style.backgroundColor = 'var(--color_2)';
+        currentItem.style.backgroundColor = 'var(--color_back)';
+        currentItem = item;
+    }
+}
+
+function changeHeight(item, h) {
+    let parent = item.parentElement;
+    if (parent != 'diagram') {
+        let pH = Number(parent.getAttribute('data-heightmemo')) + h;
+        parent.setAttribute('data-heightmemo', pH);
+        parent.style.height = pH + 'px';
+        changeHeight(parent, h);
+    }
+}
+
+document.querySelector('.wrap').addEventListener('dblclick', (e) => {
+    if (e.target.style.height == nodeH + 'px') {
+        let h = e.target.getAttribute('data-heightmemo');
+        e.target.style.height = h + 'px';
+        changeHeight(e.target, (h - nodeH))
+
+    } else {
+        e.target.style.height = nodeH + 'px'
+        let h = e.target.getAttribute('data-heightmemo');
+        changeHeight(e.target, -(h - nodeH))
+    }
+})
+
+function chnageData(name) {
+    let list = document.getElementById('diagram');
+    list.childNodes[0].remove();
+
+    goDeeper(diagram, name)
+    checkHeight(diagram)
+    breadcrumbs.innerHTML = 'Strona głowna'
+}
